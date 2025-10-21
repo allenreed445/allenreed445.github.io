4eb3e4dab4a250f5b2882ae8d29adf8a200024bf
allenreed445 <allenree255@gmail.com>
2025-10-21 10:19:41 -0500

    Assignment 5 - Carousel

--- site.js
+++ site.js
@@ -15,3 +15,42 @@ document.querySelector('#welcome').textContent = welcomeMessage // display the m
 localStorage.setItem("It's a secret to everybody.", "You gained 5 rupees!")
 
 console.log(localStorage.getItem("It's a secret to everybody."))
+
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+showImages()
+
+const prevButton = document.querySelector('#prev')
+const nextButton = document.querySelector('#next')  
+
+nextButton.addEventListener('click', () => {
+    currentImage++
+    showImages()
+})
+
+prevButton.addEventListener('click', () => {
+    currentImage--
+    showImages()
+})
+
+setInterval(() => {
+    currentImage++
+    showImages()
+}, 5000)    
