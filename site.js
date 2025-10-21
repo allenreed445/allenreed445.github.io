efa941fb8d3c13fa9c93406c55b8ac7895800756
allenreed445 <allenree255@gmail.com>
2025-10-21 09:39:22 -0500

    commit

--- site.js
+++ site.js
@@ -4,10 +4,14 @@ const isMorning = hours >= 4 && hours < 12 // is it morning?
 const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
 const isEvening = hours >= 17 || hours < 4 // is it evening?
 
-let welcomeMessage = '' // default message
+let welcomeMessage = ''
 
 if (isMorning) {welcomeMessage = 'Good Morning!'}
 if (isAfternoon) {welcomeMessage = 'Good Afternoon!'}   
 if (isEvening) {welcomeMessage = 'Good Evening!'}
 
 document.querySelector('#welcome').textContent = welcomeMessage // display the message in the div with id 'welcome'
+
+localStorage.setItem("It's a secret to everybody.", "You gained 5 rupees!")
+
+console.log(localStorage.getItem("It's a secret to everybody."))
